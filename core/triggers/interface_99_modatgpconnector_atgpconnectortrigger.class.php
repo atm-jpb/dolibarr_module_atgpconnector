57138562d914a8f9b2fad3c419fa5360a887dd2d
jpb <jean-pascal.boudet@atm-consulting.fr>
2021-01-19 16:23:15 +0100

    fix run_trigger params

--- core/triggers/interface_99_modatgpconnector_atgpconnectortrigger.class.php
+++ core/triggers/interface_99_modatgpconnector_atgpconnectortrigger.class.php
@@ -130,7 +130,7 @@ class Interfaceatgpconnectortrigger
      * 	@param		conf		$conf		Object conf
      * 	@return		int						<0 if KO, 0 if no triggered ran, >0 if OK
      */
-    public function run_trigger($action, $object, $user, $langs, $conf)
+    public function run_trigger($action, $object, User $user, Translate $langs, Conf $conf)
     {
         // Put here code you want to execute when a Dolibarr business events occurs.
         // Data and type of action are stored into $object and $action
